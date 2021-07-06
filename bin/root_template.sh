923c3496c6be8930d86b829013104404395ca32a
devtea <tdreyer1@gmail.com>
2021-07-06 09:37:52 -0500

    root template

---
+++ bin/root_template.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# template so I can remember how to elevate a script to root easily
+
+echo "Checkin' for root"
+if [[ $EUID -ne 0 ]]; then
+    echo "Relaunching as root"
+    sudo "$0" "$@"
+fi
